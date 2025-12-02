a216de9372c82bde2d823cd3af42c4b1fac6b8e4
ailuschenko059 <ailuschenko@gmail.com>
2025-12-03 00:05:47 +0500

    "Управление автотестами с Pytest.Интеграция Pytest и Playwright"

--- playwright_registration.py
+++ playwright_registration.py
@@ -28,6 +28,6 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
     page = context.new_page()
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
     page.wait_for_timeout(3000)
